3ce274654fa13b3b52e750d2fea4bf7ec947a5b3
vladislav <vldslvbusiness@gmail.com>
2024-12-16 23:57:26 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,28 @@
-from flask import Flask
-from threading import Thread
+import json
+import os
+import time
+from http.server import BaseHTTPRequestHandler
 
-app = Flask(__name__)
+from telebot import types
 
-@app.route('/')
-def index():
-    return "Бот живой"
+from create_bot.py import bot
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
 
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+class handler(BaseHTTPRequestHandler):
+    server_version = 'WebhookHandler/1.0'
+
+    def do_GET(self):
+        time.sleep(1.5)
+        bot.set_webhook('https://' + os.environ['VERCEL_URL'])
+        self.send_response(200)
+        self.end_headers()
+
+    def do_POST(self):
+        cl = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(cl)
+        body = json.loads(post_data.decode())
+
+        bot.process_new_updates([types.Update.de_json(body)])
+
+        self.send_response(204)
+        self.end_headers()
