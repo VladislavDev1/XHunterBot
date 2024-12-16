31bcee70c9eba54bb5ba98016ae19fa214d17c22
vladislav <vldslvbusiness@gmail.com>
2024-12-17 00:22:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ import handlers.start_handler
 import handlers.create_bot.add_bot
 import os
 
+import telebot
+import os
+from flask import Flask, request
+
+
+# Инициализация Flask-приложения
+app = Flask(__name__)
+
+
 ADMIN_ID = token=os.environ.get('ADMIN_ID')
 
+
+# Маршрут для Webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+# Проверочный маршрут (опционально)
+@app.route('/', methods=['GET'])
+def home():
+    return "Бот работает!", 200
+
+
 if __name__ == "__main__":
-    bot.infinity_polling()
+    # Настройка Webhook при запуске сервера
+    WEBHOOK_URL = f"x-hunter-bot-git-main-vladislavs-projects-a7fa5156.vercel.app/webhook"
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
