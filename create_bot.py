da9c021a5d4627eb7e99c6b11a1905e68ac0a5e7
Vladislav <neivo@vladislav.dev>
2026-03-18 13:19:16 +0500

    update create_bot.py for keep_alive file

--- create_bot.py
+++ create_bot.py
@@ -2,11 +2,11 @@ import asyncio
 import os
 from telebot.async_telebot import AsyncTeleBot
 from Database import Database
-# from keep_alive import keep_alive
+from keep_alive import keep_alive
 from dotenv import load_dotenv
 
 load_dotenv()
-# keep_alive() 
+keep_alive() 
 
 # Инициализация базы данных должна происходить в асинхронной функции
 # db = Database('./database.db')  # СТАРЫЙ КОД - удалить
